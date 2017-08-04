ee0e8f97871a02e5bac8377a4ffbb0b6c0b6e0eb
slrc1 <info@slrc1.comli.com>
2017-08-04 20:21:34 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\nOK\r\n"
-while { echo -en "$RESPONSE"; } | nc -l $1; do
-  echo "================================================"
-done
+while true ; do nc -l -p $PORT -c 'echo -e "HTTP/1.1 200 OK\n\n $(date)"'; done
