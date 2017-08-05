065e6aa5be614266458876a214f0d7da411b86c6
slrc1 <info@slrc1.comli.com>
2017-08-05 11:48:04 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,5 +3,13 @@ index=`cat index.html`
 
 while true
 do
-echo "HTTP/1.1 200 OK\r\n\r\n$index" | netcat -l $PORT
+ffin=$RANDOM-$RANDOM-ffin
+ffout=$RANDOM-$RANDOM-ffout
+mkfifo $ffin
+mkfifo $ffout
+while true; do cat $ffout; done | netcat -l $PORT > $ffin &
+echo "HTTP/1.1 200 OK\r\n\r\n$index" > $ffout
+cat $ffin > $ffout
+rm $ffin
+rm $ffout
 done
